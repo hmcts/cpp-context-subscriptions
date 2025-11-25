def906d5dfd886d503cc1ff3d17489a6ee819457
Sahil Arora <sahilarora@Scrumconnect-Sahil-A.local>
2025-11-25 15:57:21 +0000

    Update core-domain

--- runIntegrationTests.sh
+++ runIntegrationTests.sh
@@ -2,9 +2,10 @@
 
 CONTEXT_NAME=subscriptions
 
-FRAMEWORK_LIBRARIES_VERSION=17.101.2
-FRAMEWORK_VERSION=17.101.6
-EVENT_STORE_VERSION=17.101.5
+FRAMEWORK_LIBRARIES_VERSION=$(mvn help:evaluate -Dexpression=framework-libraries.version -q -DforceStdout)
+FRAMEWORK_VERSION=$(mvn help:evaluate -Dexpression=framework.version -q -DforceStdout)
+EVENT_STORE_VERSION=$(mvn help:evaluate -Dexpression=event-store.version -q -DforceStdout)
+FILE_SERVICE_VERSION=$(mvn help:evaluate -Dexpression=file-service.version -q -DforceStdout)
 
 DOCKER_CONTAINER_REGISTRY_HOST_NAME=crmdvrepo01
 
